bf8f431fb25958c7c214851bb00f9f5ca14fcdc8
kleber-matos <klebermatos2019@gmail.com>
2025-05-14 10:03:13 -0300

    feat: add card section

---
+++ src/components/Card/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  nome: String,
+  imagem: String,
+  preco: Number,
+});
+</script>
+
+<template>
+  <div class="card">
+    <img :src="imagem" :alt="nome" />
+    <h2>{{ nome }}</h2>
+    <span>{{ preco }},00</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  width: 100%;
+  max-width: 350px;
+  box-sizing: border-box;
+  transition: transform 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+}
+
+img {
+  width: 100%;
+}
+</style>
--- src/components/Main/index.vue
+++ src/components/Main/index.vue
@@ -1,14 +1,29 @@
 <script setup>
 import produtos from "../../data/produtos.json";
+
+import Card from "../Card/index.vue";
 </script>
 
 <template>
-  <h2>lista de produtos - teste</h2>
-
-  <div v-for="(produto, index) in produtos" :key="index">
-    <img :src="produto.imagem" />
-    <h2>{{ produto.nome }}</h2>
+  <div class="container">
+    <Card
+      v-for="(produto, index) in produtos"
+      :key="index"
+      :nome="produto.nome"
+      :imagem="produto.imagem"
+      :preco="produto.preco"
+    />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  margin: 5rem 0 5rem 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 5rem;
+  padding: 1rem;
+  box-sizing: border-box;
+}
+</style>
